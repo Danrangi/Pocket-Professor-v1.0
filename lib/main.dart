121a7c8020dec03ca3dfd7322c6898380bf99707
Ahmad Muh'd-Danrangi <159925950+Danrangi@users.noreply.github.com>
2025-05-18 08:36:50 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
-<PASTE YOUR main.dart CONTENT HERE>
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PocketProfessorApp());
+}
+
+class PocketProfessorApp extends StatefulWidget {
+  const PocketProfessorApp({Key? key}) : super(key: key);
+
+  @override
+  State<PocketProfessorApp> createState() => _PocketProfessorAppState();
+}
+
+class _PocketProfessorAppState extends State<PocketProfessorApp> {
+  bool _isDarkMode = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PocketProfessor',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
+      home: ChatScreen(
+        isDarkMode: _isDarkMode,
+        onToggleTheme: () {
+          setState(() {
+            _isDarkMode = !_isDarkMode;
+          });
+        },
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+// Add your ChatScreen class code below or import it if in another file
